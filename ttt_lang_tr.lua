683429e112af64657804e6a70688893f8ae49bb0
Matt <49911006+NovaDiablox@users.noreply.github.com>
2023-09-27 09:50:53 +0300

    Create ttt_lang_tr.lua
    Create ttt_lang_tr.lua

---
+++ ttt_lang_tr.lua
@@ -0,0 +1,32 @@
+local L = LANG.CreateLanguage("Türkçe")
+
+-- Role names
+L["innocent"] = "Masum"
+L["detective"] = "Dedektif"
+L["traitor"] = "Hain"
+L["spectator"] = "İzleyici"
+
+-- Additional phrases
+L["this_round_has"] = "Bu rauntta"
+L["round_name"] = " "
+L["round_and"] = " ve "
+
+L["symbol"]= ": "
+L["symbol2"]= "s"
+L["symbol3"]= ","
+L["symbol4"]= "."
+L["symbol5"]= ""
+L["round_player_info"] = "Bu raunttaki rol dağılımı"
+L["innocent"] = "Masum"
+L["traitor"] = "Hain"
+L["detective"] = "Dedektif"
+
+-- Colors
+L["color_innocent"] = Color(0, 255, 0, 255)
+L["color_detective"] = Color(0, 0, 255, 255)
+L["color_traitor"] = Color(255, 0, 0, 255)
+L["color_spectator"] = Color(255, 255, 0, 255)
+
+-- Save the language
+LANG.AddToLanguage("Türkçe", L)
+-- Translation by Matt STEAM URL: https://steamcommunity.com/profiles/76561198443702005
